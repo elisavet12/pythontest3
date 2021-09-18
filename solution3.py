3c4aa82942278896c3a5615fb0cd7df836a7ccff
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2021-09-18 17:41:57 +0300

    solution3

---
+++ solution3.py
@@ -0,0 +1,24 @@
+# Write a Python program to get the the volume of a sphere with radius 8.
+pi = 3.1415926535897931
+r=8.0
+x=4
+y=3
+V=(x/y)*pi*(r**y)
+print('The volume of the sphere is: ',V)
+
+# Modulo Operator with integers
+
+# 1. Print the modulo 2 of 'x'
+x=23
+y=2
+a=x%y
+print(a)
+
+
+# 2. Print the modulo 5.5 of 'y'
+y = 12.5
+x=2
+b=y%x
+print(b)
+
+
